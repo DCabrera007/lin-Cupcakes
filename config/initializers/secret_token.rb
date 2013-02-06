c915da860ba682b63c26f8cd7c224d2b18bdcbb9
Daniel Cabrera <dcabrera@ubuntu.(none)>
2013-02-06 14:25:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cupcakes::Application.config.secret_token = 'cf110015e7d8bedbb1c34f664ae0d55f7ce51d22ce2642f05196d2127d5deb3ab066e605161dd485bc08ae33cb5a31969c0b9dd86efa463b88da108f07d8c737'
