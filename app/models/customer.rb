d7cd331f4a158683069744d8c3571c6702b1ba45
Daniel Cabrera <dcabrera@ubuntu.(none)>
2013-02-20 18:17:43 -0500

    val

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ActiveRecord::Base
   attr_accessible :address, :email, :firstname, :lastname, :phonenumber
- # validates :address, :email, :firstname, :lastname, :phonenumber, presence:true
+  validates :lastname, presence:true
 end
