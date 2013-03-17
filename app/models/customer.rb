55eb6eca2ff12fe3d13b152a3a59238e93b09876
Daniel Cabrera <dcabrera@ubuntu.(none)>
2013-03-17 00:30:33 -0400

    hw9

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,13 @@
 class Customer < ActiveRecord::Base
   attr_accessible :address, :email, :firstname, :lastname, :phonenumber
+
+  scope :alpha, order("lastname asc") 
+
+  
   validates :lastname, presence:true
   validates :firstname, presence:true
   validates :phonenumber, presence:true
   validates :email, presence:true
   validates :address, presence:true
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
   attr_accessible :Date, :Description, :Name
+  scope :recent, order("created_at asc").limit(5)
 end
